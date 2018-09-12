917d3164eb3e5d5df3c2702cbd152681ac0289a6
Muriel Gonzalez <murielgonzalez@gmail.com>
2018-09-11 21:56:09 -0500

    creating observables

--- creating-observables/src/main/kotlin/main.kt
+++ creating-observables/src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
-fun main(args: Array<String>) {
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.toObservable
 
+fun main(args: Array<String>) {
+    exampleOf("creating an observable") {
+        val mostPopular: Observable<String> = Observable.just(episodeV)
+        val ogTrilogy = Observable.just(episodeIV, episodeV, episodeVI)
+        val prequelTrilogy = Observable.just(listOf(episodeI, episodeII, episodeIII))
+        val sequelTrilogy = Observable.fromIterable(listOf(episodeVII, episodeVIII, episodeIX))
+        val stories = listOf(solo, rogueOne).toObservable()
+    }
 }
